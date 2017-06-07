aa82ad8c70835179543f32076e9d5e903bb9a5a0
Ivan Zinchenko <evandersar@ukr.net>
2017-06-07 14:36:11 +0000

    done

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ var path = require('path');
 var upload = multer({ dest: 'uploads/' });
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'client')));
+
 app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname,'/client/index.html'));
 });
 
-app.post('/get-file-size', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file
+app.post('/get-file-size', upload.single('userFile'), function (req, res, next) {
+  // req.file is the `userFile` file
   // req.body will hold the text fields, if there were any
-  
+  var fSize = req.file ? req.file.size : 'undefined';
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({size: fSize}));
 });
 
 app.listen(8080, function () {
